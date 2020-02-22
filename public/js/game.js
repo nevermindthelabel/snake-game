44aa5cde5fbbd95480ce5d2c7a8db9bac5577060
Matt Kilcup <nevermindthelabel@gmail.com>
2020-02-21 21:18:28 -0700

    imported MainScene class

--- public/js/game.js
+++ public/js/game.js
@@ -1,10 +1,11 @@
-console.log('connected');
+import MainScene from "./MainScene.js";
+
  const config = {
   width: 640,
   height: 640,
   type: Phaser.AUTO,
   parent: 'game',
-  scene: []
+  scene: [MainScene]
  };
 
  new Phaser.Game(config);
