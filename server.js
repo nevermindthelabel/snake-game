b864d00f72a91f82083a394114ddd05b077ac720
Matt Kilcup <nevermindthelabel@gmail.com>
2020-02-21 21:00:19 -0700

    updated express for static assets

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require('express')
 
 const app = express();
 
+app.use(express.static(__dirname + '/public'));
+
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html')
 });
 
-app.listen(PORT, () => console.log('server running'));
+app.listen(PORT, () => console.log(`server running on port http://localhost:${PORT}`));
