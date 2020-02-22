933440e09a542b8aa5a0d9ae65fc47625307ccbd
Matt Kilcup <nevermindthelabel@gmail.com>
2020-02-21 21:37:41 -0700

    updated Snake class

--- public/js/Snake.js
+++ public/js/Snake.js
@@ -1,9 +1,10 @@
 export default class Snake {
   constructor(scene){
     this.scene = scene;
-    this.scene.add.rectangle(10, 10, 16, 16, 0xff0000);
+    this.body = [];
+    this.box = this.scene.add.rectangle(0, 0, 16, 16, 0xff0000).setOrigin(0);
   }
   update(time){
-
+    this.box.x += 1;
   }
 }
