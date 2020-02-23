f14bb09ae19d4e8c94d23827e03ea03ebf05a43d
Matt Kilcup <nevermindthelabel@gmail.com>
2020-02-22 20:24:16 -0700

    pushing different sections on to snake body

--- public/js/Snake.js
+++ public/js/Snake.js
@@ -1,10 +1,11 @@
 export default class Snake {
-  constructor(scene){
+  constructor(scene) {
     this.scene = scene;
     this.body = [];
-    this.box = this.scene.add.rectangle(0, 0, 16, 16, 0xff0000).setOrigin(0);
+    this.body.push(this.scene.add.rectangle(0, 0, 16, 16, 0xff0000).setOrigin(0));
+    this.body.push(this.scene.add.rectangle(0, 0, 16, 16, 0x0000ff).setOrigin(0));
   }
-  update(time){
-    this.box.x += 1;
+  update(time) {
+    this.body[0].x += 1;
   }
 }
