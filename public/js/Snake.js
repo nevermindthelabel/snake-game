e84823332268c92c3b0b813d3fe90aefb4453dc1
Matt Kilcup <nevermindthelabel@gmail.com>
2020-02-22 20:38:52 -0700

    added switch cases for keypress

--- public/js/Snake.js
+++ public/js/Snake.js
@@ -1,27 +1,33 @@
 export default class Snake {
   constructor(scene) {
     this.scene = scene;
+    this.direction = Phaser.Math.Vector2.RIGHT;
     this.body = [];
     this.body.push(this.scene.add.rectangle(0, 0, 16, 16, 0xff0000).setOrigin(0));
     this.body.push(this.scene.add.rectangle(0, 0, 16, 16, 0x0000ff).setOrigin(0));
-    scene.input.keyboard.on('keydown', e => { this.keydown(e)})
+    scene.input.keyboard.on('keydown', e => { this.keydown(e) })
   }
 
   keydown(event) {
     console.log(event)
-    switch(event.keyCode) {
+    switch (event.keyCode) {
       case 37: // left
-      break
+        this.direction = Phaser.Math.Vector2.LEFT;
+        break
       case 38: // up
-      break
+        this.direction = Phaser.Math.Vector2.UP;
+        break
       case 39: // right
-      break
+        this.direction = Phaser.Math.Vector2.RIGHT;
+        break
       case 40: // down
-      break
+        this.direction = Phaser.Math.Vector2.DOWN;
+        break
     }
   }
 
   update(time) {
-    this.body[0].x += 1;
+    this.body[0].x += this.direction.x;
+    this.body[0].y += this.direction.y;
   }
 }
