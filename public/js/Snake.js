e943508cdf8a12728c87702587a7703756196240
Matt Kilcup <nevermindthelabel@gmail.com>
2020-02-22 20:45:48 -0700

    added move logic

--- public/js/Snake.js
+++ public/js/Snake.js
@@ -1,6 +1,8 @@
 export default class Snake {
   constructor(scene) {
     this.scene = scene;
+    this.lastMoveTime = 0;
+    this.moveInterval = 1000;
     this.direction = Phaser.Math.Vector2.RIGHT;
     this.body = [];
     this.body.push(this.scene.add.rectangle(0, 0, 16, 16, 0xff0000).setOrigin(0));
@@ -27,7 +29,13 @@ export default class Snake {
   }
 
   update(time) {
-    this.body[0].x += this.direction.x;
-    this.body[0].y += this.direction.y;
+    if(time >= this.lastMoveTime + this.moveInterval){
+      this.lastMoveTime = time;
+      this.move();
+    }
+  }
+  move() {
+    this.body[0].x += this.direction.x * 16;
+    this.body[0].y += this.direction.y * 16;
   }
 }
