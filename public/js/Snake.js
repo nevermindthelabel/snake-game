489d840d1db48e79d9b51f810760d03f0734759e
Matt Kilcup <nevermindthelabel@gmail.com>
2020-02-22 20:28:44 -0700

    added keyboard event

--- public/js/Snake.js
+++ public/js/Snake.js
@@ -4,7 +4,13 @@ export default class Snake {
     this.body = [];
     this.body.push(this.scene.add.rectangle(0, 0, 16, 16, 0xff0000).setOrigin(0));
     this.body.push(this.scene.add.rectangle(0, 0, 16, 16, 0x0000ff).setOrigin(0));
+    scene.input.keyboard.on('keydown', e => { this.keydown(e)})
   }
+
+  keydown(event) {
+    console.log(event)
+  }
+
   update(time) {
     this.body[0].x += 1;
   }
