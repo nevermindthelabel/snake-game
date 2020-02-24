b7e552cabb86ffb184f0cb6ce339982852066ea4
Matt Kilcup <nevermindthelabel@gmail.com>
2020-02-23 17:46:27 -0700

    added logic for eating apple

--- public/js/Snake.js
+++ public/js/Snake.js
@@ -6,15 +6,15 @@ export default class Snake {
     this.tileSize = 16;
     this.direction = Phaser.Math.Vector2.RIGHT;
     this.body = [];
-    this.body.push(this.scene.add.rectangle(this.scene.game.config.width /2, this.scene.game.config.height /2, this.tileSize, this.tileSize, 0xff0000).setOrigin(0));
+    this.body.push(this.scene.add.rectangle(this.scene.game.config.width / 2, this.scene.game.config.height / 2, this.tileSize, this.tileSize, 0xff0000).setOrigin(0));
     scene.input.keyboard.on('keydown', e => { this.keydown(e) });
     this.apple = this.scene.add.rectangle(0, 0, this.tileSize, this.tileSize, 0x00ff00).setOrigin(0);
     this.positionApple();
   }
 
   positionApple() {
-    this.apple.x = Math.random() * this.scene.game.config.width;
-    this.apple.y = Math.random() * this.scene.game.config.height;
+    this.apple.x = Math.floor((Math.random() * this.scene.game.config.width) / this.tileSize) * this.tileSize;
+    this.apple.y = Math.floor((Math.random() * this.scene.game.config.height) / this.tileSize) * this.tileSize;
   }
 
   keydown(event) {
@@ -42,11 +42,18 @@ export default class Snake {
     }
   }
   move() {
+    let x = this.body[0].x + this.direction.x * this.tileSize;
+    let y = this.body[0].y + this.direction.y * this.tileSize;
+
+    if (this.apple.x === x && this.apple.y === y) {
+      console.log('eating')
+    }
+
     for (let i = this.body.length - 1; i > 0; i--) {
       this.body[i].x = this.body[i - 1].x;
       this.body[i].y = this.body[i - 1].y;
     }
-    this.body[0].x += this.direction.x * 16;
-    this.body[0].y += this.direction.y * 16;
+    this.body[0].x += x;
+    this.body[0].y += y;
   }
 }
